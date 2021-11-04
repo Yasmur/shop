a4b3556684d557c20a2be793503cc577b35b63a7
Your Name <your_email@whatever.com>
2021-11-04 21:39:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+      </header>
+    </div>
+  );
+}
+
+export default App;
