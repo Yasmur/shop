994ecaeb71545f8267d10bd56f0794fe79e7d576
Your Name <your_email@whatever.com>
2021-11-05 18:22:16 +0200

    created a data model and tested a request

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 require('dotenv').config();
 const express = require('express');
-
-
+const models = require('./models/models')
+const sequelize = require('./db')
 const PORT = process.env.PORT || 5000;
+const cors = require('cors')
+
 
 const app = express();
+app.use(cors())
+app.use(express.json())
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.get('/', (req, res) => {
+    res.status(200).json( {message: 'WORNING!!!'})
+})
+
+const start = async () => {
+    try {
+       await sequelize.authenticate()
+       await sequelize.sync()
+        app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+    }catch(e){
+        console.log(e)
+    }
+}
+
+start();
