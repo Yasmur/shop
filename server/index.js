56d08be39636bcad397010f5f58da723589a99ba
Your Name <your_email@whatever.com>
2021-11-09 22:00:22 +0200

    add middleware

--- server/index.js
+++ server/index.js
@@ -5,12 +5,16 @@ const sequelize = require('./db')
 const PORT = process.env.PORT || 5000;
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler =  require('./middleware/ErrorHandlingMiddleware')
 
 const app = express();
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
+
+//Оброботка ошибок
+app.use(errorHandler)
 
 
 const start = async () => {
