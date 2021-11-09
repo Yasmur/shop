5d6430310af86ede556c7e572e19978bf6f34dec
Your Name <your_email@whatever.com>
2021-11-09 21:15:48 +0200

    added routing and breakpoints

--- server/index.js
+++ server/index.js
@@ -4,15 +4,14 @@ const models = require('./models/models')
 const sequelize = require('./db')
 const PORT = process.env.PORT || 5000;
 const cors = require('cors')
-
+const router = require('./routes/index')
 
 const app = express();
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
 
-app.get('/', (req, res) => {
-    res.status(200).json( {message: 'WORNING!!!'})
-})
+
 
 const start = async () => {
     try {
