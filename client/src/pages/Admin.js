231c8b624786d7b3356a8cf44b6e2bd1203a31d4
Your Name <your_email@whatever.com>
2021-11-11 13:09:04 +0200

    problem with Switch

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Admin = () => {
+    return (
+        <div>
+            sdwewe
+        </div>
+    );
+};
+
+export default Admin;
---
+++ client/src/pages/Shop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            sdwewe
+        </div>
+    );
+};
+
+export default Shop;
